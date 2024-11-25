7b1365a8a2f0c557b1ebe2ca10550caac4710f43
Harun Rasit Pekacar <harun.pekacar@gmail.com>
2024-11-24 21:28:52 -0600

    Initial Commit

---
+++ SmartEnglishLearning/SmartEnglishLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartEnglishLearningApp.swift
+//  SmartEnglishLearning
+//
+//  Created by Harun Rasit Pekacar on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SmartEnglishLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
